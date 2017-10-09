29258931100ee264eabc139bc514f09cd3687771
Israel Nnaji <nnajiisrael@gmail.com>
2017-10-09 09:53:00 +0100

    resolved the pdb, xml error by deleting them

--- ICA/Admin/Manage.aspx.cs
+++ ICA/Admin/Manage.aspx.cs
@@ -13,8 +13,10 @@ namespace ICA.Admin
     public partial class Manage : System.Web.UI.Page
     {
         string cs = ConfigurationManager.ConnectionStrings["icaname"].ConnectionString;
+
         DataTable _memCategoryReport = new DataTable();
         DataTable _appStatus = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -26,8 +28,6 @@ namespace ICA.Admin
 
                 OracleDataAdapter da;
 
-                //try
-                //{
                 OracleCommand cmd = new OracleCommand("GETMEMCATLIST", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add("CUR", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
@@ -65,7 +65,71 @@ namespace ICA.Admin
 
         protected void manageRegID_Click(object sender, EventArgs e)
         {
-            //
+            //to generate registration details based on selected values from the drop down.
+            string appStatus = appStatusID.Value;
+            string memTypes = memberCatID.Value;
+
+            DataTable dt = null;
+
+            try
+            {
+
+                dt = GetRegistrationDetails(appStatus, memTypes);
+
+
+                if (dt != null && dt.Rows.Count >= 0)
+                {
+                    string html = ""; int count = 1;
+                    foreach (DataRow item in dt.Rows)
+                    {
+                        html += "<tr><td>" + (count++) + "</td><td>" + item["FULLNAME"] + "</td><td>" + item["USERNAME"] + "</td><td>" + item["MEMBERCATEGORY"] + "</td><td>" + item["APPLICATION STATUS"] + "</td></tr>";
+                    }
+
+
+                    t_body.InnerHtml = html;
+                    //gvData.DataSource = dt;
+                    //gvData.DataBind();
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('No Record Found...');</script>");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+
+
+        public DataTable GetRegistrationDetails(string _appStatus, string _memberTypes)
+        {
+
+            DataTable dt = new DataTable();
+
+            OracleConnection conn = new OracleConnection(cs);
+            conn.Open();
+
+            OracleDataAdapter da;
+
+            OracleCommand cmd = new OracleCommand("GET_PENDING_USERS", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new OracleParameter("V_FLAG", OracleDbType.Int32, _appStatus, ParameterDirection.Input));
+            cmd.Parameters.Add(new OracleParameter("V_MEMTYPE", OracleDbType.Int32, _memberTypes, ParameterDirection.Input));
+            
+            cmd.Parameters.Add("CUR", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+
+            da = new OracleDataAdapter(cmd);
+            da.Fill(dt);
+
+            return dt;
+
         }
     }
 }
